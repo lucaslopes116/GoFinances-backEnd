e58224cd9a4a9f0c23456a69e4d21a152f01fb9a
lopes <lopes@arquia.com>
2020-05-18 13:27:33 -0300

    fix nome da coluna da table

--- src/database/migrations/1589817242168-CreateCategories.ts
+++ src/database/migrations/1589817242168-CreateCategories.ts
@@ -24,7 +24,7 @@ export default class CreateCategories1589817242168
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
